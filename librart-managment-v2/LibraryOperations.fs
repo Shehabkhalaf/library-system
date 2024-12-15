3788e560cbe26d281f8d60bf6c32e297b1fc460a
belalhanafy <belalhanafy373@gmail.com>
2024-12-15 20:04:18 +0200

    Implemented my functions in libraryoperations

---
+++ librart-managment-v2/LibraryOperations.fs
@@ -0,0 +1,24 @@
+module LibraryOperations
+
+open System
+open Data
+
+
+let addBook title author genre =
+    loadBooks()
+    let id = (books |> Map.toList |> List.length) + 1
+    let book = { Id = id; Title = title; Author = author; Genre = genre; Status = "Available"; BorrowDate = None }
+    books <- books.Add(id, book)
+    saveBooks ()
+
+
+let borrowBook id =
+    loadBooks()
+    match books.TryFind id with
+    | Some(book) when book.Status = "Available" -> 
+        let updatedBook = { book with Status = "Borrowed"; BorrowDate = Some(DateTime.Now.ToString("yyyy-MM-dd")) }
+        books <- books.Add(id, updatedBook)
+        saveBooks ()
+        true
+    | Some(_) -> false
+    | None -> false
